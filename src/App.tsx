e29ce23910f077efcc6c00320e5e7c8a9686014e
chioooo <mduran43@gmail.com>
2025-12-10 08:45:14 -0600

    mantener la sesion

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,29 @@ import { RegisterScreen } from "./components/RegisterScreen";
 import { DashboardScreen } from "./components/DashboardScreen";
 
 export default function App() {
-    const [currentScreen, setCurrentScreen] = useState<"login" | "register" | "dashboard">("login");
+    const [currentScreen, setCurrentScreen] = useState<"login" | "register" | "dashboard">(
+        () => {
+            return (localStorage.getItem("currentScreen") as anyy) || "login";
+        }
+    );
+
+    // Cada vez que cambie la pantalla, guardamos en localStorage
+    const updateScreen = (screen: "login" | "register" | "dashboard") => {
+        setCurrentScreen(screen);
+        localStorage.setItem("currentScreen", screen);
+    };
 
     return (
         <>
             {currentScreen === "login" ? (
                 <LoginScreen
-                    onGoToRegister={() => setCurrentScreen("register")}
-                    onLogin={() => setCurrentScreen("dashboard")}
+                    onGoToRegister={() => updateScreen("register")}
+                    onLogin={() => updateScreen("dashboard")}
                 />
             ) : currentScreen === "register" ? (
-                <RegisterScreen onBackToLogin={() => setCurrentScreen("login")} />
+                <RegisterScreen onBackToLogin={() => updateScreen("login")} />
             ) : (
-                <DashboardScreen onLogout={() => setCurrentScreen("login")} />
-
+                <DashboardScreen onLogout={() => updateScreen("login")} />
             )}
         </>
     );
