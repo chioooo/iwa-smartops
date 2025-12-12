681a54ae6dfd2defe9638cce03e3908401a03ef7
Rocio Duran <119270392+chioooo@users.noreply.github.com>
2025-12-12 10:19:06 -0600

    DEMO-104 (#3)
    * users service and data
    
    * mantener la sesion
    
    * modificación de tabla
    
    * eliminar permisos de operaciones
    
    * no contraseña temporal
    
    * quitar doble y
    
    * modal de crear rol
    
    * reutilizar modal de crear usuario para editar
    
    * cambiar tipos
    
    * editar usuario
    
    * agregar roleId
    
    * modal header style
    
    * password required
    
    * nombre de rol requerido
    
    * mostrar el rol asignado al editar usuario, contador funcional de usuarios en roles y permisos
    
    * tooltip y modal de confirmación
    
    * eliminar usuario
    
    * feedback
    
    * flecha
    
    * flecha en modal
    
    ---------
    
    Co-authored-by: chioooo <mduran43@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,28 @@ import { RegisterScreen } from "./components/RegisterScreen";
 import { DashboardScreen } from "./components/DashboardScreen";
 
 export default function App() {
-    const [currentScreen, setCurrentScreen] = useState<"login" | "register" | "dashboard">("login");
+    const [currentScreen, setCurrentScreen] = useState<"login" | "register" | "dashboard">(
+        () => {
+            return (localStorage.getItem("currentScreen") as any) || "login";
+        }
+    );
+
+    const updateScreen = (screen: "login" | "register" | "dashboard") => {
+        setCurrentScreen(screen);
+        localStorage.setItem("currentScreen", screen);
+    };
 
     return (
         <>
             {currentScreen === "login" ? (
                 <LoginScreen
-                    onGoToRegister={() => setCurrentScreen("register")}
-                    onLogin={() => setCurrentScreen("dashboard")}
+                    onGoToRegister={() => updateScreen("register")}
+                    onLogin={() => updateScreen("dashboard")}
                 />
             ) : currentScreen === "register" ? (
-                <RegisterScreen onBackToLogin={() => setCurrentScreen("login")} />
+                <RegisterScreen onBackToLogin={() => updateScreen("login")} />
             ) : (
-                <DashboardScreen onLogout={() => setCurrentScreen("login")} />
-
+                <DashboardScreen onLogout={() => updateScreen("login")} />
             )}
         </>
     );
